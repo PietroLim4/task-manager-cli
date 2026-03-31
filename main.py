99ee963751944676c8afb0dd467d414fff7445a1
PietroLim4 <pietro.lima604@gmail.com>
2026-03-31 17:41:21 -0300

    feat: add task removal and save to file

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
+def show_tasks():
+    print("--- Task List ---")
+    for i in range(len(tasks)):
+        status = "[X]" if tasks[i][1] else "[ ]"
+        print(i+1, "-", tasks[i][0], status)
 tasks=[]
+
+if os.path.exists("tasks.txt"):
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            text, status = line.strip().split("|")
+            tasks.append([text, status == "True"])
+            
 while True:
     clear()
     print("--- Task Manager ---")
     print("1 - Add Tasks")
     print("2 - List Tasks")
     print("3 - Mark As Done")
+    print("4 - Remove Task")
     print("0 - Exit")
 
     op = input("\nChoose: ")
@@ -23,24 +35,12 @@ while True:
 
     elif op=="2":
         clear()
-        print("--- Task List ---")
-        for i in range(len(tasks)):
-            if tasks[i][1] == True:
-                status = "[X]"
-            else:
-                status = "[ ]"
-            print(i+1, "-", tasks[i][0], status)
+        show_tasks()
         input("\nPress Enter to continue...")
 
     elif op=="3":
         clear()
-        print("--- Task List ---")
-        for i in range(len(tasks)):
-            if tasks[i][1] == True:
-                status = "[X]"
-            else:
-                status = "[ ]"
-            print(i+1, "-", tasks[i][0], status)
+        show_tasks()
         try:
             cho = int(input("Choose a task to mark/desmark: "))
 
@@ -53,9 +53,28 @@ while True:
         except:
             print("\nInvalid input")
         input("\nPress Enter to continue...")
+
+    elif op=="4":
+        clear()
+        show_tasks()
+        try:
+            cho = int(input("Choose a task to remove: "))
+
+            if 1 <= cho <= len(tasks):
+                removed = tasks.pop(cho-1)
+                print(f"\nRemoved: {removed[0]}")
+            else:
+                print("\nTask does not exist")
+
+        except:
+            print("\nInvalid input")
+        input("\nPress Enter to continue...")
        
 
     elif op=="0":
+        with open("tasks.txt", "w") as file:
+            for task in tasks:
+                file.write(task[0] + "|" + str(task[1]) + "\n")
         break
     else:
         print("\nInvalid command.")

7e484389b51373f5ce43ca127325d49208b92b51
PietroLim4 <pietro.lima604@gmail.com>
2026-03-31 17:24:25 -0300

    feat: add status and status modifier.

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ def clear():
 tasks=[]
 while True:
     clear()
+    print("--- Task Manager ---")
     print("1 - Add Tasks")
     print("2 - List Tasks")
+    print("3 - Mark As Done")
     print("0 - Exit")
 
-    op = input("Choose: ")
+    op = input("\nChoose: ")
 
     if op=="1":
         task=input("\nTask: ")
-        tasks.append(task)
+        tasks.append([task, False])
         print("Task added!")
         input("\nPress Enter to continue...")
 
@@ -23,8 +25,35 @@ while True:
         clear()
         print("--- Task List ---")
         for i in range(len(tasks)):
-            print(i+1, "-", tasks[i])
+            if tasks[i][1] == True:
+                status = "[X]"
+            else:
+                status = "[ ]"
+            print(i+1, "-", tasks[i][0], status)
         input("\nPress Enter to continue...")
+
+    elif op=="3":
+        clear()
+        print("--- Task List ---")
+        for i in range(len(tasks)):
+            if tasks[i][1] == True:
+                status = "[X]"
+            else:
+                status = "[ ]"
+            print(i+1, "-", tasks[i][0], status)
+        try:
+            cho = int(input("Choose a task to mark/desmark: "))
+
+            if 1 <= cho <= len(tasks):
+                tasks[cho-1][1] = not tasks[cho-1][1]
+                print("\nChange done.")
+            else:
+                print("\nTask does not exist")
+
+        except:
+            print("\nInvalid input")
+        input("\nPress Enter to continue...")
+       
 
     elif op=="0":
         break
